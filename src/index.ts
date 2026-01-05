60d5edef2e77a9ef9d0afc8f035442728fb8764c
atomu okada gyawali <atomgyawali2@gmail.com>
2026-01-05 15:36:21 +0545

    sprint 1 final

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { PORT } from "./config/index";
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import { connectDatabase } from "./database/mongodb";
+import authRoutes from "./routes/auth.routes";
 
 const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/api/auth', authRoutes);
 app.get("/", (req: Request, res: Response) => {
   return res
     .status(200)
